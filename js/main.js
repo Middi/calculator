4778eaf48d45a1314b20f94b2e4c045dee528e3c
“Richard Middleton” <Richard@richardmiddleton.me>
2017-04-13 17:17:21 +0200

    all keys and hovers, added js clock

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Add zero in front of minutes if less than 10
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+// Get Hours and Mins
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    // Send mins to checktime incase it needs a zero
+    m = checkTime(m);
+    // Add to dom
+    document.getElementById('status-bar').innerHTML =
+    "<p>" + h + ":" + m + "</p>";
+    // Run every 500milliseconds
+    setTimeout(startTime, 500);
+}
+
+startTime();
